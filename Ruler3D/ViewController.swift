b70ac2079042b3aafe4f0f90d67aa2881677183b
Ionut-Catalin Bolea <silicson@gmail.com>
2018-03-07 09:14:52 +0100

    Latest changes

--- Ruler3D/ViewController.swift
+++ Ruler3D/ViewController.swift
@@ -14,20 +14,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    
+    var textNode = SCNNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,34 +47,95 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    //MARK: - Detect user touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            dotNodes.forEach{ (dot) in
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    //MARK: - Dot rendering methods
+    func addDot(at hitResult: ARHitTestResult) {
+        
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let dotMaterial = SCNMaterial()
+        
+        dotMaterial.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials.append(dotMaterial)
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(
+            x: hitResult.worldTransform.columns.3.x,
+            y: hitResult.worldTransform.columns.3.y,
+            z: hitResult.worldTransform.columns.3.z
+        )
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculateDistance()
+        }
+        
+    }
+    
+    
+    //MARK: - Calculate distances between 2 points
+    func calculateDistance() {
+        let startNodePosition = dotNodes[0].position
+        let endNodePosition = dotNodes[1].position
+        
+        //d = sqrt( a^2 + b^2 + c^2 )
+        
+        let distance = sqrt(
+            pow(startNodePosition.x - endNodePosition.x, 2) +
+            pow(startNodePosition.y - endNodePosition.y, 2) +
+            pow(startNodePosition.z - endNodePosition.z, 2)
+        )
+        
+        generateText3D(withNumber: distance, atPosition: endNodePosition)
+    }
+    
+    func generateText3D(withNumber number:Float, atPosition position: SCNVector3) {
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: "\(number) meters", extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.white
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = position
+        
+        textNode.scale = SCNVector3(x: 0.003, y: 0.003, z: 0.003)
+        
+        if let camera = sceneView.pointOfView {
+            textNode.orientation = camera.orientation
+        }
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
+        
+        
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
